47cd00da409a543f0bc826758df9b1366ad8bc73
peng wan <mrpengwan@gmail.com>
2015-03-30 00:38:25 -0700

    merge

---
+++ js/common.js
@@ -0,0 +1,48 @@
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+
+function html_encode(str)
+{
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&/g, "&amp;");
+    s = s.replace(/</g, "&lt;");
+    s = s.replace(/>/g, "&gt;");
+    s = s.replace(/ /g, "&nbsp;");
+    s = s.replace(/\'/g, "&#39;");
+    s = s.replace(/\"/g, "&quot;");
+    s = s.replace(/\n/g, "<br>");
+    return s;
+}
+
+function html_decode(str)
+{
+    var s = "";
+    if (str.length == 0) return "";
+    s = str.replace(/&amp;/g, "&");
+    s = s.replace(/&lt;/g, "<");
+    s = s.replace(/&gt;/g, ">");
+    s = s.replace(/&nbsp;/g, " ");
+    s = s.replace(/&#39;/g, "\'");
+    s = s.replace(/&quot;/g, "\"");
+    s = s.replace(/<br>/g, "\n");
+    return s;
+}
+
+function returnFloat(value){
+    var value=Math.round(parseFloat(value)*100)/100;
+    var xsd=value.toString().split(".");
+    if(xsd.length==1){
+        value=value.toString()+".0";
+        return value;
+    }
+    if(xsd.length>1){
+        if(xsd[1].length<1){
+            value=value.toString()+"0";
+        }
+        return value;
+    }
+}
